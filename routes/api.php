95dd9fc34f38d02c0e269e6cd8129ec032c44167
Peter Mangoro <petertmangoro@gmail.com>
2024-08-15 15:42:53 +0200

    queueing

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Jobs\SendEmailJob;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/send-email', function (Request $request) {
+
+
+    $email = request()->email;
+
+    SendEmailJob::dispatch($email)->onQueue('emails');
+    return response()->json("Email queued to be sent to {$email}", 200);
+});
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Events\AddedNewCustomer;
 use App\Events\Example;
 use App\Events\PackageSent;
+use App\Jobs\SendEmailJob;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -19,6 +21,7 @@ Route::middleware([
     })->name('dashboard');
 });
 
-Route::get('/broadcast', function () {
-    broadcast(new PackageSent('Peter', 'Test', '00.00.00'));
+Route::get('/test', function () {
+    $user= User::first();
+    SendEmailJob::dispatch($user)->onQueue('emails');
 });
